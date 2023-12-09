233fd811596e5fe632db8edfb7334b35d409d186
Franco10 <Frannchu_lopez@hotmail.com>
2023-12-08 21:31:14 -0300

    Se agrega el Proyecto Final

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import Contacto from "./components/Contacto";
+import ItemDetailContainer from "./components/ItemDetailContainer";
+import ItemListContainer from "./components/ItemListContainer";
+import Navbar from "./components/Navbar";
+import Nosotros from "./components/Nosotros";
+import "./main.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CartProvider } from "./context/CartContext";
+import Carrito from "./components/Carrito";
+import Checkout from "./components/Checkout";
+
+function App() {
+
+  return (
+
+    <CartProvider>
+
+      <BrowserRouter>
+
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/Item/:id" element={<ItemDetailContainer />} />
+          <Route path="productos" element={<ItemListContainer />} />
+          <Route path="productos/:categoria" element={<ItemListContainer />} />
+          <Route path="/nosotros" element={<Nosotros />} />
+          <Route path="/contacto" element={<Contacto />} />
+          <Route path="/carrito" element={<Carrito />} />
+          <Route path="/checkout" element={<Checkout />} />
+
+        </Routes>
+
+      </BrowserRouter>
+
+    </CartProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CartWidget from './CartWidget';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <Link to="/" className="logo"> <h1>Bazaar.F.L</h1></Link>
+            <ul className="menu">
+                <li><Link className="menu-link" to="/">Inicio</Link></li>
+                <li><Link className="menu-link" to="/productos">Productos</Link></li>
+                <li><Link className="menu-link" to="/productos/baño">Baño</Link></li>
+                <li><Link className="menu-link" to="/productos/cocina">Cocina</Link></li>
+                <li><Link className="menu-link" to="/productos/vasos y tazas">Vasos y Tazas</Link></li>
+                <li><Link className="menu-link" to="/nosotros">Nosotros</Link></li>
+                <li><Link className="menu-link" to="/contacto">Contacto</Link></li>
+                <li><CartWidget /></li>
+
+            </ul>
+        </nav>
+    )
+}
+
+export default Navba
